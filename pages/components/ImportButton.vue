cb4c8930fe8ec71b36f06bf9dce5147715f539d9
Qiu Jun <qiujun2004@outlook.com>
2025-05-28 12:50:00 +0800

    feat: Import button first commit

---
+++ pages/components/ImportButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const value = ref('')
+const fileName = ref('')
+const fileContent = ref('')
+
+function handleFileChange(event) {
+  const file = event.target.files[0]
+  if (file) {
+    fileName.value = file.name
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      fileContent.value = e.target.result
+    }
+    reader.readAsText(file)
+  }
+}
+</script>
+
+<template>
+  <UInput v-model="value" type="file" @change="handleFileChange" />
+  <div>选择的文件: {{ fileName }}</div>
+  <div>文件内容: {{fileContent}}</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <UApp>
+    <ImportButton />
+  </UApp>
+</template>
+
+<script setup lang="ts">
+import ImportButton from "~/pages/components/ImportButton.vue";
+</script>

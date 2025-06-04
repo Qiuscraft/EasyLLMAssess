86daf088c101bced08004bdcfb67af9179fa7200
Qiu Jun <qiujun2004@outlook.com>
2025-06-04 10:46:05 +0800

    feat: rename Name to Dataset Name in DatasetsTable in order to prevent pop "personal informations" in Edge Browser.

--- components/dataset/DatasetsTable.vue
+++ components/dataset/DatasetsTable.vue
@@ -85,7 +85,7 @@ const columns: TableColumn<Dataset>[] = [
         h("label", {
           class: "pointer-events-none absolute left-0 -top-2.5 text-highlighted text-xs font-medium px-1.5 transition-all peer-focus:-top-2.5 peer-focus:text-highlighted peer-focus:text-xs peer-focus:font-medium peer-placeholder-shown:text-sm peer-placeholder-shown:text-dimmed peer-placeholder-shown:top-1.5 peer-placeholder-shown:font-normal",
         }, [
-          h("span", { class: "inline-flex bg-default px-1" }, "Name")
+          h("span", { class: "inline-flex bg-default px-1" }, "Dataset Name")
         ])
       ])
     },

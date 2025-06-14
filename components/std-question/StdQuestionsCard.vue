53d5fcec657479a74b41e11ae54a07cd2d40f7ec
Qiu Jun <qiujun2004@outlook.com>
2025-06-14 19:49:11 +0800

    feat: version change for StdQuestionsCard.vue

--- components/std-question/StdQuestionsCard.vue
+++ components/std-question/StdQuestionsCard.vue
@@ -1,35 +1,76 @@
 <script setup lang="ts">
-
-import {type StdQuestionVersion} from "~/server/types/mysql";
+import {type StdQuestion, type StdQuestionVersion} from "~/server/types/mysql";
 
 const props = defineProps(['questions']);
-const selectedVersion = ref<Record<number, StdQuestionVersion>>({});
+const versionSortedQuestions = computed(() => {
+  return sortStdQuestionVersionsByCreationTime(props.questions);
+})
+const selectedVersionString = ref<Record<number, string>>({});
+
+const versionOptions = computed(() => {
+  const options: Record<number, string[]> = {};
+  if (versionSortedQuestions.value) {
+    for (const question of versionSortedQuestions.value) {
+      options[question.id] = question.versions.map(v => `${v.version}`);
+    }
+  }
+  return options;
+});
+
+const selectedVersion = computed<Record<number, StdQuestionVersion>>(() => {
+  const versions: Record<number, StdQuestionVersion> = {};
+  if (versionSortedQuestions.value) {
+    for (const question of versionSortedQuestions.value) {
+      const versionString = selectedVersionString.value[question.id];
+      versions[question.id] = getVersionByString(question, versionString) || question.versions[0];
+    }
+  }
+  return versions;
+});
 
 watch(props.questions, () => {
-  selectedVersion.value = {};
+  selectedVersionString.value = {};
   if (props.questions) {
-    for (const question of props.questions) {
-      if (question.versions && question.versions.length > 0) {
-        let latestVersion = question.versions[0];
-        for (const version of question.versions) {
-          if (version.createdAt > latestVersion.createdAt) {
-            latestVersion = version;
-          }
-        }
-        selectedVersion.value[question.id] = latestVersion;
-      }
+    for (const question of versionSortedQuestions.value) {
+      selectedVersionString.value[question.id] = question.versions[0].version;
     }
   }
 }, { immediate: true });
+
+function sortStdQuestionVersionsByCreationTime(questions: StdQuestion[]): StdQuestion[] {
+  if (!questions) {
+    return [];
+  }
+  return questions.map(question => {
+    const sortedVersions = [...question.versions].sort((a, b) => {
+      return a.createdAt.getTime() - b.createdAt.getTime();
+    });
+    return { ...question, versions: sortedVersions };
+  });
+}
+
+function getVersionByString(question: StdQuestion, versionString: string): StdQuestionVersion | undefined {
+  return question.versions.find(v => v.version === versionString);
+}
 </script>
 
 <template>
   <UPageCard title="Standard Questions">
     <UPageCard
       v-for="sq in props.questions"
-      :title="`Standard Question #${sq.id}-${selectedVersion[sq.id].version}: ${selectedVersion[sq.id].content}`"
       :description="`Standard Answer: ${selectedVersion[sq.id].answer.content}`"
     >
+      <template #header>
+        <div class="flex items-center gap-2">
+          <span>Standard Question #{{sq.id}}-</span>
+          <USelect
+              v-model="selectedVersionString[sq.id]"
+              :items="versionOptions[sq.id]"
+              class="w-48"
+          />
+          <span>: {{selectedVersion[sq.id].content}}</span>
+        </div>
+      </template>
       <UTable
         :data="selectedVersion[sq.id].answer.scoringPoints"
         :columns="[

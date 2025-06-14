a6e1ab835fe33735966736fa83a94dafd8664daa
Qiu Jun <qiujun2004@outlook.com>
2025-06-14 21:21:42 +0800

    fix: handle many errors for StandardQuestionsTable.vue

--- utils/std-question.ts
+++ utils/std-question.ts
@@ -1,4 +1,5 @@
 import type {StdQuestion, StdQuestionVersion} from "~/server/types/mysql";
+import type {SerializeObject} from "nitropack";
 
 export function sortStdQuestionVersionsByCreationTime(questions: StdQuestion[]): StdQuestion[] {
     if (!questions) {
@@ -14,4 +15,27 @@ export function sortStdQuestionVersionsByCreationTime(questions: StdQuestion[]):
 
 export function getVersionByString(question: StdQuestion, versionString: string): StdQuestionVersion | undefined {
     return question.versions.find(v => v.version === versionString);
+}
+
+export function responseToStdQuestions(questions: SerializeObject<StdQuestion>[]): StdQuestion[] {
+    if (!questions) {
+        return [];
+    }
+    return questions.map(question => {
+        // question is of type SerializeObject<StdQuestion>
+        // question.versions should be of type SerializeObject<StdQuestionVersion>[]
+        const deserializedVersions = question.versions.map(version => {
+            // version is of type SerializeObject<StdQuestionVersion>
+            // version.createdAt is expected to be a string
+            return {
+                ...version,
+                createdAt: new Date(version.createdAt as string),
+            } as StdQuestionVersion;
+        });
+
+        return {
+            ...question,
+            versions: deserializedVersions,
+        } as StdQuestion;
+    });
 }
